6a36a1dca81fee4be3b6597eaae0844a24d90090
tdenis29 <tdenis29@outlook.com>
2021-05-29 12:35:32 -0700

    almostGoter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+
 /*onload needs to load starter grid and begin listener.
 then on reset we need to 
 1. erase colored squares ? function eraseSquares ? classlist maybe needed to handle rainbow squares.
@@ -7,10 +8,16 @@ then on reset we need to
 4.start listening
 */
 const container = document.getElementById("container");
+//buttons
 const reset = document.getElementById('reset');
-const clearer = document.getElementById('clear')
+const clearer = document.getElementById('clear');
+const rainbow = document.getElementById('rainbow');
+const pen = document.getElementById('pen');
+//raibow getter returns a color
+
+
 // this makes the grid for strarter and reset 
-function makeRows(rows, cols) {
+function makeRows(rows = 16, cols = 16) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
@@ -19,9 +26,11 @@ function makeRows(rows, cols) {
   };
   const gridCollection = document.getElementsByClassName('grid-item');
   const gridItems = Array.from(gridCollection);
+  //listen to draw
   gridItems.forEach(item => {
     item.addEventListener('mouseover', (e) =>{
-        item.style.backgroundColor = "grey";
+        item.classList.toggle('colored');
+        item.style.backgroundColor = pen.value;
         // add class to toggle for draw/erase? 
     })
 });
@@ -34,6 +43,8 @@ function eraseSquares(){
     gridItems.forEach(item => {
         if(item.style.backgroundColor === "grey"){
             item.style.backgroundColor = "white";
+        } if(item.style.backgroundColor !== "white"){
+            item.style.backgroundColor = "white";
         }
     })
 }
@@ -45,22 +56,25 @@ clearer.addEventListener('click', e => {
 reset.addEventListener("click", (e) =>{
     let newGrid = prompt("Enter a number between 16-100");
     let newGridInt = parseInt(newGrid);
-    if(newGridInt > 100 || newGridInt === NaN){
-        alert("Number is too big or not a number! C'MON MAN!")
+    if(newGridInt > 100 || newGridInt === NaN || newGridInt === 0 || newGridInt === ""){
+        alert("Number is too big or not a number! C'MON DUDE! 0 doesnt count!")
+        makeRows(16,16)
     }
     else{
         container.innerHTML = "";
         makeRows(newGridInt, newGridInt);
+        pen.value = "#808080";
     }
 })
-//get grid items we could make this a function or add to above function.
-// const gridCollection = document.getElementsByClassName('grid-item');
-// //get array because collection sucks
-// const gridItems = Array.from(gridCollection);
-
-// //nested event listener 
-// gridItems.forEach(item => {
-//     item.addEventListener('mouseover', (e) =>{
-//         item.style.backgroundColor = "grey";
-//     })
-// });
+//wait for clicks on rainbow 
+rainbow.addEventListener('click', e =>{
+    eraseSquares();
+    const gridCollection = document.getElementsByClassName('grid-item');
+    const gridItems = Array.from(gridCollection);
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', (e) =>{
+            let newColor = "#";
+            newColor += Math.floor(Math.random()*16777215).toString(16);
+            item.style.backgroundColor = newColor;
+        })
+})});
